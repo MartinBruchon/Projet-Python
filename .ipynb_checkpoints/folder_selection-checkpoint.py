679c7b5b225b398285b9aed48cc9ca0b2dc5403a
larkaa <maildrewmurphy@gmail.com>
2023-12-19 17:47:23 +0100

    updated pdf & img functions

---
+++ .ipynb_checkpoints/folder_selection-checkpoint.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import customtkinter as ctk
+import main_page
+
+global dossier
+dossier = ""
+
+def choisir_dossier():
+    dossier = ctk.filedialog.askdirectory()
+    if dossier:
+        root.destroy()
+        main_page.main(dossier)
+
+# Créer la fenêtre principale
+root = ctk.CTk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+root.title("Page d'accueil")
+
+# Créer et placer les widgets
+label_instructions = ctk.CTkLabel(root, text="Choisissez un dossier local :", font=('',20))
+label_instructions.pack(pady=10)
+
+button_choisir = ctk.CTkButton(root, text="Choisir un dossier", command=choisir_dossier, font=('',20))
+button_choisir.pack(pady=20)
+
+# Lancer l'application
+root.eval('tk::PlaceWindow . center')
+root.mainloop()
