e9ea59735736395c6a02f7a8e08824c60ee53283
Martin Bruchon <martin.bruchon@ecam-strasbourg.eu>
2023-12-20 19:52:12 +0100

    interaction between main page and choosen folder : scan, analyse, display

--- main_page.py
+++ main_page.py
@@ -1,8 +1,29 @@
 import tkinter as tk
 from customtkinter import *
 from scan_folder import scan
+from PIL import Image
 
 colors = ["#1C1C1C" , "#282828"]
+size=(96, 96)
+pdf_img = Image.open("./src/pdf.png")
+pdf_img = CTkImage(light_image=pdf_img, dark_image=pdf_img, size=size)
+img_img = Image.open("./src/img.png")
+img_img = CTkImage(light_image=img_img, dark_image=img_img, size=size)
+ppt_img = Image.open("./src/ppt.png")
+ppt_img = CTkImage(light_image=ppt_img, dark_image=ppt_img, size=size)
+word_img = Image.open("./src/word.png")
+word_img = CTkImage(light_image=word_img, dark_image=word_img, size=size)
+xls_img = Image.open("./src/xls.png")
+xls_img = CTkImage(light_image=xls_img, dark_image=xls_img, size=size)
+sound_img = Image.open("./src/sound.png")
+sound_img = CTkImage(light_image=sound_img, dark_image=sound_img, size=size)
+video_img = Image.open("./src/video.png")
+video_img = CTkImage(light_image=video_img, dark_image=video_img, size=size)
+warn_img = Image.open("./src/warn.png")
+warn_img = CTkImage(light_image=warn_img, dark_image=warn_img, size=size)
+
+def test():
+    print("ok")    
 
 def main(dossier):
     root = CTk()
@@ -23,14 +44,25 @@ def main(dossier):
 
     for key, values in filetype_list.items():
                 
-        label = CTkLabel(frame, text=key)
-        label.pack(padx=20)
+        label = CTkLabel(frame, text=key, font=('', 20), anchor='w')
+        label.pack(fill=X, padx=100)
         sub_frame = CTkFrame(frame, bg_color=colors[1])
         sub_frame.pack(padx=100, pady=20, fill=X, expand=True)
         
         for i,files in enumerate(values) :
+
+            if key == "pdf" : panel = CTkButton(sub_frame, fg_color="transparent", text="", image=pdf_img, command=test)
+            elif key in ["png", "jpg" ,"jpeg"] : panel = CTkButton(sub_frame, fg_color="transparent", text="", image=img_img, command=test)
+            elif key in ["ppt", "pptx"] : panel = CTkButton(sub_frame, fg_color="transparent", text="", image=ppt_img, command=test)
+            elif key in ["doc", "docx"] : panel = CTkButton(sub_frame, fg_color="transparent", text="", image=word_img, command=test)
+            elif key in ["xls", "xlsx"] : panel = CTkButton(sub_frame, fg_color="transparent", text="", image=xls_img, command=test)
+            elif key in ["mp3"] : panel = CTkButton(sub_frame, fg_color="transparent", text="", image=sound_img, command=test)
+            elif key in ["mp4"] : panel = CTkButton(sub_frame, fg_color="transparent", text="", image=video_img, command=test)
+            else : panel = CTkButton(sub_frame, fg_color="transparent", text="", image=warn_img, command=test)
+            panel.grid(row=0, column=i, padx=10)
+            
             file_label = CTkLabel(sub_frame, text=files)
-            file_label.grid(row=0, column=i, padx=10)
+            file_label.grid(row=1, column=i, padx=10)
         
     def on_frame_configure(event):
         canvas.configure(scrollregion=canvas.bbox("all"))
@@ -39,3 +71,5 @@ def main(dossier):
 
     root.title("main")
     root.mainloop()
+    
+main("./test_dir")
--- scan_folder.py
+++ scan_folder.py
@@ -1,5 +1,21 @@
 import os
 
+import filetype
+def check_file_integrity(file_path):
+    kind = filetype.guess(file_path)
+    message = ''
+    if kind is None:
+        message = "Unknown or unsupported image type."
+    actual_type = kind.extension
+    file_extension = os.path.splitext(file_path)[1].lower().strip('.')
+
+    if actual_type == file_extension:
+        message = True
+    else:
+        message = False
+    
+    return actual_type, file_extension, message
+
 class dict(dict):
     def add_file(self, type, file):
         if type in files_list.keys():
@@ -11,11 +27,13 @@ class dict(dict):
 files_list = dict()
 
 def get_file_type(file):
-    type = file.split(".")[-1]
-    return type
+    real_type, ext, integrity = check_file_integrity(file)
+    return real_type, integrity
 
 def scan(dossier):
     for file in os.listdir(dossier):
-        file_type = get_file_type(file)
-        files_list.add_file(file_type, file)
+        file_type, integrity = get_file_type(dossier + "/" + file)
+        if integrity == 1 :
+            files_list.add_file(file_type, file)
+        else : files_list.add_file("INTEGRITY NOT SURE", file)
     return files_list
