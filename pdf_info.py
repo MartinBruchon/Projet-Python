8f7b821b50a1ede602c255d2897906fe595170c1
Martin Bruchon <martin.bruchon@ecam-strasbourg.eu>
2023-12-29 13:28:31 +0000

    minor changes

--- pdf_info.py
+++ pdf_info.py
@@ -1,10 +1,22 @@
 from customtkinter import *
 from PIL import Image
 from metadata_functions import get_pdf_metadata as pdf
-from pdf2image import convert_from_path
 from platform import system
+from pdf2jpg import pdf2jpg
+import shutil
 
 colors = ["#1C1C1C" , "#282828"]
+
+def fit(img, panel):
+    img_size = img.size
+    panel_size = (panel.winfo_width(), panel.winfo_height())
+    img_ratio = img_size[0]/img_size[1]
+    panel_ratio = panel_size[0]/panel_size[1]
+    
+    if img_ratio <= panel_ratio :
+        return (panel_size[1]*img_ratio, panel_size[1])
+    else :
+        return (panel_size[0], panel_size[0]//img_ratio)
 
 def metadata_parser(metadatas):
     txt = ""
@@ -22,6 +34,11 @@ def main(file):
     metadatas = pdf(file)
     txt = metadata_parser(metadatas)
     
+    res = pdf2jpg.convert_pdf2jpg(file, ".", pages="0")
+    dir, f = res[0]["output_pdfpath"], res[0]["output_jpgfiles"][0]
+    img = Image.open(f)
+    shutil.rmtree(dir)
+        
     root = CTkToplevel()
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -33,13 +50,21 @@ def main(file):
     
     frame = CTkFrame(root, fg_color=colors[0])
     frame.pack(fill=BOTH, expand=True)
-    frame.grid_rowconfigure([0,1], weight=1, uniform="row")
+    frame.grid_rowconfigure(0, weight=1, uniform="row")
     frame.grid_columnconfigure([0,1], weight=1, uniform='col')
             
     data_panel = CTkLabel(frame, bg_color=colors[1], text=txt, anchor=NW, justify=LEFT, padx=20, pady=20)
     data_panel.grid(column=0, rowspan=2, sticky=NSEW, padx = 30, pady=30)
     data_panel.update()
     data_panel.configure(wraplength = data_panel.winfo_width()-100)
+    
+    preview_panel = CTkLabel(frame, bg_color=colors[1], text='')
+    preview_panel.grid(row=0, column=1, sticky=NSEW,  padx = 30, pady=30)
+    
+    preview_panel.update()
+    size = fit(img, preview_panel)
+    img = CTkImage(light_image=img, dark_image=img, size=size)
+    preview_panel.configure(image=img, anchor=CENTER, padx=0, pady=0)    
     
     if txt == "There is no metadata in this file.\nTry another one." :
         data_panel.configure(justify=CENTER, anchor=CENTER)
