92153112f6abb5403fad16cef7a45d48ce688644
Martin Bruchon <martin.bruchon@ecam-strasbourg.eu>
2023-12-31 14:41:00 +0000

    documentation

--- load_saved.py
+++ load_saved.py
@@ -5,6 +5,12 @@ from json import dumps
 from tkinter import Text 
 
 def read(file):
+    """Load, read and display the content of the previously saved JSON file
+    Args:
+        file (str): Path of the JSON file
+    """    
+    
+    # Create the window
     colors = ["#1C1C1C", "#282828"]
     set_appearance_mode("dark")
     root = CTk()
@@ -17,14 +23,15 @@ def read(file):
     else:
         root.state("zoomed")
 
+    # Get the datas from JSON as String
     data = dumps(load_and_print(file), indent=2)
 
     text_widget = Text(root, wrap='word', bg=colors[1], fg='white', borderwidth=-1, padx=20, pady=20)
-    text_widget.pack(fill=BOTH, expand=True, side=LEFT)  # Pack to fill the window and expand
+    text_widget.pack(fill=BOTH, expand=True, side=LEFT)
 
-    scrollbar = CTkScrollbar(root, command=text_widget.yview)  # Create the scrollbar
-    scrollbar.pack(side=RIGHT, fill=Y)  # Pack it to the right side
-    text_widget.configure(yscrollcommand=scrollbar.set)  # Link scrollbar to the text widget
+    scrollbar = CTkScrollbar(root, command=text_widget.yview)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    text_widget.configure(yscrollcommand=scrollbar.set)
 
     text_widget.configure(state='normal')
     text_widget.insert('1.0', data)
@@ -34,7 +41,8 @@ def read(file):
 
 
 def main():
-    
+    """Open a file dialog to visualize JSON files and to select the one the user want to load
+    """    
     file = filedialog.askopenfile(filetypes=[("JSON files", "*.json")])
     if file:
         read(file.name)   
--- scan_folder.py
+++ scan_folder.py
@@ -1,8 +1,9 @@
 import os
 import filetype
 
+# Conversion dictionnary between the type of the file and its category
 type_parser = {
-    "Image" : ["png","jpg","jpeg"],
+    "Image" : ["png","jpg","jpeg", "gif"],
     "PowerPoint Presentation" : ["ppt", "pptx"],
     "Excel" : ["xls", "xlsx"],
     "Word Document" : ["doc", "docx"],
@@ -12,22 +13,40 @@ type_parser = {
 }
 
 def get_key_from_extension(extension):
+    """Get the class in function of the extension of the file
+    Args:
+        extension (str): The extension of the file. ex:'png'
+    Returns:
+        str: The class of the file. ex:'Image'
+    """    
     for key, value in type_parser.items():
         if extension in value:
             return key
     return None
 
 def get_filetype(file_path):
+    """Get the real type of a file
+    Args:
+        file_path (str): The path of the file
+    Returns:
+        str: The real type of the file
+    """    
     return filetype.guess(file_path).extension
 
 def check_file_integrity(file_path):
+    """Crucial method to check for the integrity of the file. Compare the extension to the realtype of the file to validate or not the integrity
+    Args:
+        file_path (str): Path to the file
+    Returns:
+        str, str, str: The real type, the extension and the integrity flag of the file
+    """    
     kind = filetype.guess(file_path)
     if kind is None:
         return None, None, False
     actual_type = kind.extension
     file_extension = os.path.splitext(file_path)[1].lower().strip('.')
     
-    if file_extension == "jpeg" : file_extension = "jpg"
+    if file_extension == "jpeg" : file_extension = "jpg" # JPEG file are JPG files
 
     if actual_type == file_extension:
         integrity = True
@@ -36,7 +55,13 @@ def check_file_integrity(file_path):
     
     return actual_type, file_extension, integrity
 
-def scan(dossier):
+def scan(folder_path):
+    """Scan the folder and return the list of the different files in this folder ordered by their type
+    Args:
+        folder_path (str): Path of the working folder
+    Returns:
+        dict: Dictionnairy of the different possible file types and the associated files of the folder
+    """    
     files_list = {"PDF Document":[],
               "Image":[], 
               "Video":[],
@@ -45,8 +70,10 @@ def scan(dossier):
               "PowerPoint Presentation":[],
               "Excel":[],
               "Integrity check failure or unsupported file":[]}
-    for file in os.listdir(dossier):
-        full_path = dossier + "/" + file
+    
+    # Scan the folder, check the integrity of the files and store the file names into the good category
+    for file in os.listdir(folder_path):
+        full_path = folder_path + "/" + file
         if os.path.isfile(full_path) :
             file_type,_,integrity = check_file_integrity(full_path)
             if integrity == 1 :
