8d0a89ed574324c86c5cc23ff59a953f591fe8a8
Martin Bruchon <martin.bruchon@ecam-strasbourg.eu>
2023-12-31 13:52:51 +0000

    documentation

--- folder_selection.py
+++ folder_selection.py
@@ -4,7 +4,11 @@ import home_page
 dossier = ""
 
 def main():
+    """Small window for working folder selection
+    """    
     def choisir_dossier():
+        """ Show a dialog window for directory selection and launch the home page with this folder
+        """        
         dossier = ctk.filedialog.askdirectory()
         if dossier:
             root.destroy()
@@ -17,13 +21,11 @@ def main():
 
     root.title("Page d'accueil")
 
-    # Créer et placer les widgets
     label_instructions = ctk.CTkLabel(root, text="Choisissez un dossier local :", font=('',20))
     label_instructions.pack(pady=10)
 
     button_choisir = ctk.CTkButton(root, text="Choisir un dossier", command=choisir_dossier, font=('',20))
     button_choisir.pack(pady=20)
 
-    # Lancer l'application
     root.eval('tk::PlaceWindow . center')
     root.mainloop()
